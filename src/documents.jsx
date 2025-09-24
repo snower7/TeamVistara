6bfb6551de8dd6c34443274c97232e6ded993200
jl7739 <109109212+snower7@users.noreply.github.com>
2025-09-24 16:19:56 -0400

    move docs to assets

--- src/documents.jsx
+++ src/documents.jsx
@@ -1,17 +1,19 @@
 import { Container } from "reactstrap";
 import Header from "./header";
 import { useState, useEffect } from "react";
+import Week1 from './assets/documents/4-ups/Week1.pdf';
 
 function Document() {
-    const [fourUps, setFourUps] = useState(import.meta.glob('/public/documents/4-ups/*.pdf', {import: 'default', eager: true}))
+    const [fourUps, setFourUps] = useState(import.meta.glob('./assets/documents/4-ups/*.pdf', {import: 'default', eager: true}))
 
     function renderFourUps() {
+        console.log(fourUps);
         var list = []
         for (const [key, value] of Object.entries(fourUps)) {
             var dir = key.split('/');
-            const stringdir = dir.filter((value) => value !== "public").join("/")
+            const stringdir = dir.filter((value) => value != ".").join("/")
 
-            list.push(<li><a href={"/TeamVistara" + stringdir}>{dir[dir.length-1]}</a></li>)
+            list.push(<li><a target="_blank" href={"/TeamVistara/src/" + stringdir}>{dir[dir.length-1]}</a></li>)
         }
         return list
     }
